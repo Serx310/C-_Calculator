5f3c92fb2e40a068c2e6e18408752fa7638619ac
Opilane TTHK <opilane@tthk.ee>
2021-09-22 14:32:44 +0300

    Made a calculator

---
+++ CSharp_Kalkulaator_SergeiBubnovTARpe20/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Kalkulaator_SergeiBubnovTARpe20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstnum;
+            double secondnum;
+            double answer=0d;
+            string ops;
+
+            Console.WriteLine("\t\t\tConsole Calculator by Sergei Bubnov from TARpe20 in TTHK\n");
+            Console.WriteLine("\t\t\t----------------------------------\r");
+
+
+            Console.WriteLine("\t\t\tEnter the first number\n");
+            firstnum = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\t\t\tSelect an operator: (+,-,/,*, **)\n");
+            ops = Console.ReadLine();
+
+            Console.WriteLine("\t\t\tEnter the second number\n");
+            secondnum = Double.Parse(Console.ReadLine());
+            if (ops=="+")
+            {
+                answer = firstnum + secondnum;
+            }else if (ops == "-")
+            {
+                answer = firstnum - secondnum;
+            }else if(ops == "/")
+            {
+                answer = firstnum / secondnum;
+            }else if(ops == "*")
+            {
+                answer = firstnum * secondnum;
+            }
+            else if (ops=="**")
+            {
+                answer = Math.Pow(firstnum, secondnum);
+            }
+            Console.WriteLine(answer);
+            Console.ReadKey();
+
+        }
+    }
+}
